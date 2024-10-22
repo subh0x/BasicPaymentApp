31d2241ebbff6c20e3699075b02ba0e0c128cb67
subh0x <sciencesub18@gmail.com>
2024-10-22 23:13:09 +0530

    Added user signin route & userModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6
+    }
+}, { timestamps: true });
+
+const User = new mongoose.model("user", userSchema);
+
+module.exports = User;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,10 @@
 import { z } from "zod";
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const JWT_SECRET = require('../config/config');
+
 const userRouter = express.Router();
 
 // Input Validation Schema:
@@ -16,40 +21,70 @@ const userSignInValidationSchema = z.object({
 });
 
 // User signup Route:
-userRouter.post('/signup', (req, res) => {
+userRouter.post('/signup', async (req, res) => {
     try {
         const { email, firstname, lastname, password } = req.body;
 
         // Input Validation:
-        const validData = userSignUpValidationSchema.safeParse(email, firstname, lastname, password);
-        
+        const validData = userSignUpValidationSchema.safeParse(req.body);
         if (!validData.success) {
-            return res.status(404).send({ errorMessage: validData.error });
+            return res.status(400).send({ errorMessage: validData.error });
         }
 
-        //TODO: Check if the email already exists in the DB or not:
-        
+        // Check if the email already exists in the DB or not:
+        const userExists = User.findOne({ email: email });
+        if (userExists) {
+            return res.status(409).send({ message: "Email already exists!" });
+        }
 
-        //TODO: If Valid New user Update/Add it to the DB.:
+        // Hash the password before saving it:
+        const hashedPassword = await bcrypt.hash(password, 10);
 
+        // If Valid New user Update/Add it to the DB.:
+        const newUser = await User.create({
+            email: email,
+            firstname: firstname,
+            lastname: lastname,
+            password: hashedPassword
+        });
+
+        const token = jwt.sign({ userId: newUser._id, email: email }, JWT_SECRET);
+
+        return res.status(200).send({
+            message: "User created successfully!",
+            userId: newUser._id,
+            token: token
+        });
 
     }
     catch (err) {
-        return res.status(400).send({ message: err.message });
+        return res.status(500).send({ message: err.message });
     }
 });
 
-// User Signin Route:
-userRouter.post('/signin', (req, res) => {
+// User signin Route:
+userRouter.post('/signin', async (req, res) => {
     try {
         const { email, password } = req.body;
 
         // Input Validation:
-        const validData = userSignInValidationSchema.safeParse(email, password);
-        
+        const validData = userSignInValidationSchema.safeParse(req.body);
         if (!validData.success) {
             return res.status(404).send({ errorMessage: validData.error });
         }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const userDetails = await User.findOne({ email: email, password: hashedPassword });
+        if (!userDetails) {
+            return res.status(404).send({ Message: "Please enter correct user details." });
+        }
+
+        const token = jwt.sign({ userId: userDetails._id, email: email }, JWT_SECRET);
+
+        return res.status(200).send({
+            token: token
+        });
+
     }
     catch (err) {
         return res.status(400).send({ message: err.message });
